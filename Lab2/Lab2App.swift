ec910d88cbc29dc3797b8073097b0cc6bb53bf04
Jeremy <jeremychen144@gmail.com>
2022-10-17 12:54:28 -0700

    Initial Commit

---
+++ Lab2/Lab2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab2App.swift
+//  Lab2
+//
+//  Created by Jeremy Chen on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Lab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
